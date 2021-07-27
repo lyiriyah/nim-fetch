d72f385d0ba0e8e722832992300d8dad40bc5a32
lyiriyah <oscar.wittamsnangle@gmail.com>
2021-07-27 12:50:29 +0100

    update

--- src/fetch.nim
+++ src/fetch.nim
@@ -1,43 +1,56 @@
-import terminal, osproc, os, math, strutils
+import terminal, osproc, os, math, strutils, tables, sequtils
 import std/exitprocs
 
 enableTrueColors()
 
 let
-  username = getEnv("USER")
-  hostname = execProcess("hostname")
-  distroname = execProcess("cat /etc/os-release | grep -oP \"(?<=^PRETTY_NAME=).+\" | tr -d \"\\\"\" ")
+  hostuser = execProcess("hostname").strip(chars={'\n'}) & "@" & getEnv("USER")
   cpuname = execProcess("lscpu | grep \"Model name\" | tr -s \" \" | awk '{for(i=3;i<=NF;++i)printf $i\"\"FS ; print \"\"}'")
   editor = getEnv("EDITOR")
   shell = execProcess("basename $(echo $SHELL)")
   architecture = execProcess("lscpu | grep Arch | tr -s \" \" | awk '{print $2}'")
   memory = execProcess("free -h | sed -n '2p' | awk '{print $2}'")
   usedmem = execProcess("printf \"%s/%s\n\" \"$(free -h | sed -n '2p' | awk '{print $3}')\" \"$(free -h | sed -n '2p' | awk '{print $2}')\"")
-  userhostlen = username.len + hostname.len
 
-stdout.styledWrite(fgGreen, username, resetStyle, "@", fgGreen, hostname)
+proc getCpuUsage(): string =
+  let firstLine = "/proc/stat".readLines(1)[0]
+  let vals = firstline.splitWhitespace()[1..^1].map(parseInt)
+  let totalTime = sum(vals)
+  let notIdlePerc = (1 - (vals[3] / totalTime)) * 100
+  return $(round(notIdlePerc, 1)) & "%\n"
 
-for c in 1..userhostlen:
-  if c == int(round(userhostlen / 2)):
-    stdout.write("\u252C")
-  elif c == userhostlen:
-    stdout.write("\u2500\n")
-  else:
-    stdout.write("\u2500")
+proc getDistro(): string =
+  let file = "cat /etc/os-release"
+  let grepcmd = r"grep -oP ""(?<=^PRETTY_NAME=).+"" "
+  let trcmd = "tr -d \"\\\"\""
+  let pipe = "|"
+  return execProcess(file & pipe & grepcmd & pipe & trcmd)
 
-stdout.styledWrite(fgBlue, indent("distro", (8 - "distro".len)), resetStyle,
-    " \u2502 ", distroname)
-stdout.styledWrite(fgBlue, indent("arch", (8 - "arch".len)), resetStyle,
-    " \u2502 ", architecture)
-stdout.styledWrite(fgBlue, indent("editor", (8 - "editor".len)), resetStyle,
-    " \u2502 ", editor & "\n")
-stdout.styledWrite(fgBlue, indent("shell", (8 - "shell".len)), resetStyle,
-    " \u2502 ", shell)
-stdout.styledWrite(fgBlue, indent("cpu", (8 - "cpu".len)), resetStyle,
-    " \u2502 ", cpuname)
-stdout.styledWrite(fgBlue, indent("memory", (8 - "memory".len)), resetStyle,
-    " \u2502 ", memory)
-stdout.styledWrite(fgBlue, indent("usage", (8 - "usage".len)), resetStyle,
-    " \u2502 ", usedmem)
+proc getArch(): string =
+   let archval = execProcess("lscpu").lines().readLines(1)[0]
+   echo archval
+getArch()
+
+let outtable = {
+  "host@user": hostuser & "\n",
+  "distro": getDistro(),
+  "arch": architecture,
+  "editor": editor & "\n",
+  "shell": shell,
+  "cpu": cpuname,
+  "cpu usage": getCpuUsage(),
+  "memory": memory,
+  "mem usage": usedmem,
+}.toOrderedTable()
+
+var outkeys = newSeq[string]()
+
+for k in keys(outtable):
+  outkeys.add(k)
+
+let largestkey = max(outkeys.mapIt(it.len))
+
+for k, v in pairs(outtable):
+    stdout.styledWrite(fgBlue, align(k, max(outkeys.mapIt(it.len))), resetStyle, " \u2502 ", v)
 
 addExitProc(resetAttributes) # restore terminal attrs
